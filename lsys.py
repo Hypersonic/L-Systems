9403259fc639add17549bbae5a8be62cca60172d
Josh Hofing <joshhofing@gmail.com>
2015-05-14 13:52:08 -0400

    Factor rendering to it's own function

--- lsys.py
+++ lsys.py
@@ -16,6 +16,27 @@ def lsys(s, rules):
             stk.pop(0)
     return "".join(out)
 
+def render(commands, ruleset={'forward_dist':10.0, 'angle':pi/2}):
+    ez = EZDraw()
+    #ez.pos = (ez.w-50,ez.h - 50)
+    nth = 0
+    stk = []
+    for sym in commands:
+        if sym == 'F':
+            ez.forward(ruleset['forward_dist'])
+        elif sym == '-':
+            ez.left(ruleset['angle'])
+        elif sym == '+':
+            ez.right(ruleset['angle'])
+        elif sym == '[':
+            stk.append((ez.pos, ez.angle))
+        elif sym == ']':
+            ez.pos, ez.angle = stk.pop()
+        nth += 1
+        if nth % 256 == 0:
+            ez.update(0)
+    return ez
+
 i = "A"
 iterations = 10
 for x in xrange(iterations):
@@ -24,27 +45,15 @@ for x in xrange(iterations):
     #i = lsys(i, {"A": "A+BF+", "B": "-FA-B"}) # dragon curve
     i = lsys(i, {"A": "B[-FA]", "B": "FA[+B]"})
 
-print i
-ez = EZDraw()
-#ez.pos = (ez.w-50,ez.h - 50)
-nth = 0
-stk = []
-for sym in i:
-    if sym == 'F':
-        ez.forward(10.0)
-    elif sym == '-':
-        ez.left(pi/5)
-    elif sym == '+':
-        ez.right(pi/5)
-    elif sym == '[':
-        stk.append((ez.pos, ez.angle))
-    elif sym == ']':
-        ez.pos, ez.angle = stk.pop()
-    nth += 1
-    if nth % 256 == 0:
-        ez.update(0)
 
 print "done"
 
+ruleset = {
+            'forward_dist': 5.0,
+            'angle': pi/5
+        }
+
+print i
+ez = render(i, ruleset)
 while True:
     ez.update(0)
